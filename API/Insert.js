ea53d1bd896b9e32d757106da10304f07d789e0b
itspaulthegreat <arindam.8599@gmail.com>
2021-03-27 20:39:04 +0530

    up

--- API/Insert.js
+++ API/Insert.js
@@ -2,7 +2,7 @@ const mongo = require('../mongo.js')
 // const mongoose = require('mongoose')
 const MySchema = require('../schemas/Schema.js')
 const mdbservice = require('../dataBaseService/mdbservice.js')
-
+const path = require('path');
 
 const putitem = async () =>{
     params = {
@@ -14,11 +14,10 @@ const putitem = async () =>{
     
     
     var userSchema = 'userSchema'
-    dbname = "hi"
+    dbname = "hi2"
     console.log("inside putitem",params)
     await mdbservice.putItemMongoDB(params,userSchema,dbname)
-    callback(null,"ji")
-
+    // res.redirect(path.join(__dirname+'/Frontend/index.html'))
 }
 
 const queryitem = async () =>{
--- index.js
+++ index.js
@@ -5,10 +5,9 @@ const router = express.Router();
 const path = require('path');
 
 const insert = require('./API/Insert');
-
-console.log(insert)
-router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/Frontend/index.html'))
+app.use(express.json());
+router.post('/',function(req,res){
+    console.log(req)
 })
 
 router.get('/about',insert.putitem)
@@ -19,7 +18,7 @@ router.get('/sitemap',function(req,res){
 
 
 app.use('/', router);
-app.use(express.static((path.join(__dirname, '/API/Insert'))))
+app.use(express.static((path.join(__dirname, '/Frontend/index.html'))))
 app.listen(process.env.PORT || 3000)
 
 console.log('Running at Port 3000');
